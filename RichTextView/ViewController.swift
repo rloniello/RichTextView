87d45ab141c3ca05afc2fb262e360ce079c85551
Russell <rloniello@gmail.com>
2022-09-08 14:12:50 -0400

    Initial Commit

---
+++ RichTextView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichTextView
+//
+//  Created by Russell on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
